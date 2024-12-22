72421e5fdd674bb2278a16bfd100cb6c1a805754
Maicon Boeira da Cunha <maicoboeira@hotmail.com>
2024-12-22 11:21:36 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("jornada de dados")
+
+#Variáveis
+idade = 42
+nome = "Maicon"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5,6,7,8]
+print(lista)
+
+lista_invertida = lista.reverse()
+print(lista_invertida)
